8c9bb858b0323c27d268842f40223c2db27f5750
wxterfall <23og2704@barton.ac.uk>
2024-08-04 14:44:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace TextRank_Algorithm
+{
+    public class Graph
+    {
+        private Dictionary<string, List<string>> adjList;
+        public Graph()
+        {
+            adjList = new Dictionary<string, List<string>>();
+        }
+        public void addEdge(string kw1, string kw2)
+        {
+            if (!adjList.ContainsKey(kw1)) adjList[kw1] = new List<string>();
+            if (!adjList.ContainsKey(kw2)) adjList[kw2] = new List<string>();
+            if (!adjList[kw1].Contains(kw2)) adjList[kw1].Add(kw2);
+            if (!adjList[kw2].Contains(kw1)) adjList[kw2].Add(kw1);
+        }
+        public Dictionary<string, List<string>> getAdjList()
+        {
+            return adjList;
+        }
+        public Dictionary<string, double> calcPageRank(double dampingFactor, int iterations)
+        {
+            Dictionary<string, List<string>> adjList = getAdjList();
+            Dictionary<string, double> nodeScores = new Dictionary<string, double>();
+            foreach (string node  in adjList.Keys)
+            {
+                nodeScores[node] = 1;
+            }
+
+            for (int loops = 0; loops < iterations; loops++)
+            {
+                Dictionary<string, double> updatedScores = new Dictionary<string, double>();
+                foreach (string node in adjList.Keys)
+                {
+                    double score = (1 - dampingFactor);
+                    foreach (string n in adjList[node])
+                    {
+                        score += (dampingFactor * nodeScores[n] / adjList[n].Count); // update each nodes score based on the PageRank formula of each neighbours score
+                    }
+                    updatedScores[node] = score; 
+                }
+                 nodeScores = updatedScores;
+            }
+            return nodeScores;
+        }
+    }
+    internal class Program
+    {
+         //["a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it", "no", "not", "of", "on", "such", "that", "the", "their", "then", "there", "these", "they", "this", "to", "was", "will", "with"];
+        public static string[] stopWords = ["a", "about", "above", "after", "again", "against", "all", "also", "am", "an", "and", "any", "are", "arent", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "cant", "cannot", "could", "couldnt", "did", "didnt", "do", "does", "doesnt", "doing", "dont", "down", "during", "each", "few", "for", "from", "further", "had", "hadnt", "has", "hasnt", "have", "havent", "having", "he", "hed", "hell", "hes", "her", "here", "heres", "hers", "herself", "him", "himself", "his", "how", "hows", "i", "id", "ill", "im", "ive", "if", "in", "into", "is", "isnt", "it", "its", "its", "itself", "lets", "me", "more", "most", "mustnt", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "said", "same", "shant", "she", "shed", "shell", "shes", "should", "shouldnt", "so", "some", "such", "than", "that", "thats", "the", "their", "theirs", "them", "themselves", "then", "there", "theres", "these", "they", "theyd", "theyll", "theyre", "theyve", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasnt", "we", "wed", "well", "were", "weve", "were", "werent", "what", "whats", "when", "whens", "where", "wheres", "which", "while", "who", "whos", "whom", "why", "whys", "will", "with", "wont", "would", "wouldnt", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves"];
+        static List<string> keywordExtraction(string text)
+        {
+            double dampingFactor = 0.85;
+            int windowSize = 4;
+            int iterations = 100;
+            int topN = 5; 
+
+            string[] keywords = stopwordFilter(text); // split into single words and remove stop words
+            Graph keywordGraph = createGraph(keywords, windowSize); // create a graph containing each word connected to neighbour words
+            Dictionary<string, double> wordValues = keywordGraph.calcPageRank(dampingFactor, iterations); // calculate the score for each word based on its connection to neighbours
+
+            //Implement way to order wordvalues in desc order
+            List<KeyValuePair<string, double>> sortedWords = wordValues.OrderByDescending(score => score.Value).ToList();
+
+            List<string> finalKeywords = new List<string>();
+
+            for (int i = 0; i < Math.Min(topN, sortedWords.Count()); i++) // extracts the topN words
+            {
+                finalKeywords.Add(sortedWords[i].Key);
+            }
+
+            return finalKeywords; //return final keywords
+        }
+        static string[] stopwordFilter(string text)
+        {
+            string[] splitWords = text.ToLower().Split(new char[] {' ', ',', '.', '?', '!', ':', ';', '/'}, StringSplitOptions.RemoveEmptyEntries);
+            List<string> keywords = new List<string>();
+            foreach (string i in splitWords)
+            {
+                if (!stopWords.Contains(i))
+                {
+                    keywords.Add(i);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+ 
+            foreach (string keyword in keywords)
+            {
+                Console.WriteLine(keyword);
+            }
+            Console.WriteLine();
+
+            return keywords.ToArray();
+        }
+        static Graph createGraph(string[] keywords, int windowSize)
+        {
+            Graph graph = new Graph();
+            for (int i = 0; i < keywords.Length; i++)
+            {
+                for (int j = i + 1; j < i + windowSize && j < keywords.Length; j++)
+                {
+                    graph.addEdge(keywords[i], keywords[j]);
+                }
+            }
+            return graph;
+        }
+        static void Main(string[] args)
+        {
+            string text = "Recursion is when a subroutine calls itself.";
+            //string text = "This is a sample document, and this document is a sample text for keyword extraction using TextRank.";
+            var results = keywordExtraction(text);
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+            Console.ReadKey();
+        }
+    }
+}
