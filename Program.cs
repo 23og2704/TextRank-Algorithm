13227b0f256c1ae38ac527e1304c0b17e9d65106
23og2704 <23og2704@L3041>
2024-10-09 15:55:49 +0100

    need to implement topnumber calc to get a set amount of keywords in relation to length of input (>30 = small)

--- Program.cs
+++ Program.cs
@@ -51,21 +51,24 @@ namespace TextRank_Algorithm
     }
     internal class Program
     {
+        static int topNumberCalculator(int length)
+        {
+            throw new NotImplementedException();
+        }
          //["a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it", "no", "not", "of", "on", "such", "that", "the", "their", "then", "there", "these", "they", "this", "to", "was", "will", "with"];
-        public static string[] stopWords = { "a", "about", "above", "after", "again", "against", "all", "also", "am", "an", "and", "any", "are", "arent", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "cant", "cannot", "could", "couldnt", "did", "didnt", "do", "does", "doesnt", "doing", "dont", "down", "during", "each", "few", "for", "from", "further", "had", "hadnt", "has", "hasnt", "have", "havent", "having", "he", "hed", "hell", "hes", "her", "here", "heres", "hers", "herself", "him", "himself", "his", "how", "hows", "i", "id", "ill", "im", "ive", "if", "in", "into", "is", "isnt", "it", "its", "its", "itself", "lets", "me", "more", "most", "mustnt", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "said", "same", "shant", "she", "shed", "shell", "shes", "should", "shouldnt", "so", "some", "such", "than", "that", "thats", "the", "their", "theirs", "them", "themselves", "then", "there", "theres", "these", "they", "theyd", "theyll", "theyre", "theyve", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasnt", "we", "wed", "well", "were", "weve", "were", "werent", "what", "whats", "when", "whens", "where", "wheres", "which", "while", "who", "whos", "whom", "why", "whys", "will", "with", "wont", "would", "wouldnt", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves" };
+        public static string[] stopWords = { "a", "about", "above", "after", "again", "against", "all", "also", "am", "an", "and", "any", "are", "arent", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can", "cant", "cannot", "could", "couldnt", "did", "didnt", "do", "does", "doesnt", "doing", "dont", "down", "during", "each", "few", "for", "from", "further", "had", "hadnt", "has", "hasnt", "have", "havent", "having", "he", "hed", "hell", "hes", "her", "here", "heres", "hers", "herself", "him", "himself", "his", "how", "hows", "i", "id", "ill", "im", "ive", "if", "in", "into", "is", "isnt", "it", "its", "its", "itself", "lets", "me", "more", "most", "mustnt", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "said", "same", "shant", "she", "shed", "shell", "shes", "should", "shouldnt", "so", "some", "such", "than", "that", "thats", "the", "their", "theirs", "them", "themselves", "then", "there", "theres", "these", "they", "theyd", "theyll", "theyre", "theyve", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasnt", "we", "wed", "well", "were", "weve", "were", "werent", "what", "whats", "when", "whens", "where", "wheres", "which", "while", "who", "whos", "whom", "why", "whys", "will", "with", "wont", "would", "wouldnt", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves" };
         static List<string> keywordExtraction(string text)
         {
             double dampingFactor = 0.85;
             int windowSize = 4;
             int iterations = 100;
-            int topN = 5; 
+            int topN = topNumberCalculator(text.Length); 
 
             string[] keywords = stopwordFilter(text); // split into single words and remove stop words
             Graph keywordGraph = createGraph(keywords, windowSize); // create a graph containing each word connected to neighbour words
             Dictionary<string, double> wordValues = keywordGraph.calcPageRank(dampingFactor, iterations); // calculate the score for each word based on its connection to neighbours
 
-            //Implement way to order wordvalues in desc order
-            List<KeyValuePair<string, double>> sortedWords = wordValues.OrderByDescending(score => score.Value).ToList();
+            List<KeyValuePair<string, double>> sortedWords = wordValues.OrderByDescending(score => score.Value).ToList(); //Order words in dict by wordValues in descending order (LINQ)
 
             List<string> finalKeywords = new List<string>();
 
@@ -73,9 +76,9 @@ namespace TextRank_Algorithm
             {
                 finalKeywords.Add(sortedWords[i].Key);
             }
-
-            return finalKeywords; //return final keywords
+            return finalKeywords; 
         }
+
         static string[] stopwordFilter(string text)
         {
             string[] splitWords = text.ToLower().Split(new char[] {' ', ',', '.', '?', '!', ':', ';', '/'}, StringSplitOptions.RemoveEmptyEntries);
@@ -91,7 +94,8 @@ namespace TextRank_Algorithm
                     continue;
                 }
             }
- 
+
+            Console.WriteLine("With stopwords removed: "); //testing
             foreach (string keyword in keywords)
             {
                 Console.WriteLine(keyword);
@@ -114,9 +118,10 @@ namespace TextRank_Algorithm
         }
         static void Main(string[] args)
         {
-            string text = "Recursion is when a subroutine calls itself.";
+            string text = "Encryption is the process of using an algorithm to convert plaintext into ciphertext, a form that cannot be easily understood by a human or machine without knowing how to decrypt it.";
             //string text = "This is a sample document, and this document is a sample text for keyword extraction using TextRank.";
             var results = keywordExtraction(text);
+            Console.WriteLine("After full RAKE process:");
             foreach (var result in results)
             {
                 Console.WriteLine(result);
